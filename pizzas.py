b001c68005615f3b51de48b661f535e437c11870
ThomasDig18 <67803970+ThomasDig18@users.noreply.github.com>
2020-07-20 15:38:44 -0700

    Add files via upload

---
+++ pizzas.py
@@ -0,0 +1,17 @@
+favorite_pizza = ['regular', 'sicilian', 'pepperoni']
+
+for pizza in favorite_pizza:
+	print(pizza.title())
+	print(f"I like {pizza} pizza")
+
+print("pizza is one of my favororite meals but it is unhealthy")
+
+
+
+
+animals = ['fish', 'whales', 'seals']
+for animal in animals:
+	print(animal.title())
+	
+
+print("all of these animals can swim")
