ebad0bb24015b5f47542034251b6c2fa5577a841
ThomasDig18 <67803970+ThomasDig18@users.noreply.github.com>
2020-07-20 09:34:17 -0700

    Add files via upload

---
+++ name.py
@@ -0,0 +1,4 @@
+first_name = "ada"
+last_name = "DiGregorio"
+full_name = f"{first_name.title()} {last_name.title()}"
+print(full_name)
---
+++ name_cases.py
@@ -0,0 +1,19 @@
+v_1 = "Thomas DiGregorio"
+message = f"what is up {v_1}?"
+print(message)
+
+v_2 = "Donkey Kong"
+print(f"{v_2.upper()}")
+print(f"{v_2.lower()}")
+print(f"{v_2.title()}")
+
+famous_person = "Pain"
+quote_1 = '"There will never be peace as long as we live in this accursed world."'
+print(f"{famous_person} once stated, {quote_1}")
+
+v_3 = "   Pippin Vincenzo   "
+print(v_3)
+print(v_3.rstrip())
+print(v_3.lstrip())
+
+
