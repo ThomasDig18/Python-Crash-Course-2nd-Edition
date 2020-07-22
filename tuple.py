47feaeba7a85b0e2325c219bf9f6a4d4aa9adc61
ThomasDig18 <67803970+ThomasDig18@users.noreply.github.com>
2020-07-22 07:11:59 -0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+# 4-13
+buffet_style = ('chicken', 'ham', 'salad', 'pasta', 'tacos')
+print("This will be the menu at the buffet: ")
+for food in buffet_style:
+	print(food)
+
+buffet_style = ('Turkey', 'ham', 'salad', 'pasta', 'fajitas')
+print("This is the modified menu")
+for food in buffet_style:
+	print(food)
