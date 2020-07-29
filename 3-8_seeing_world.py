0157fb8287d3fe9fde00b56c27f6e82dc085184a
ThomasDig18 <67803970+ThomasDig18@users.noreply.github.com>
2020-07-29 11:46:45 -0700

    Add files via upload

---
+++ 3-8_seeing_world.py
@@ -0,0 +1,14 @@
+places_visit = ['Italy', 'Germany', 'France', 'Australia', 'Japan']
+
+print(places_visit)
+
+print(sorted(places_visit))
+
+places_visit.reverse()
+
+print(places_visit)
+
+print(len(places_visit))
+
+
+
