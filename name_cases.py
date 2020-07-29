c27dcb68569a182cfed6b11406d6a7abb3b01f72
ThomasDig18 <67803970+ThomasDig18@users.noreply.github.com>
2020-07-29 11:45:31 -0700

    Add files via upload

---
+++ name_cases.py
@@ -0,0 +1,19 @@
+v_1 = "Thomas DiGregorio"
+message = f"what is up {v_1}?"
+print(message)
+
+v_2 = "Donkey Kong"
+print(f"{v_2.upper()}")
+print(f"{v_2.lower()}")
+print(f"{v_2.title()}")
+
+famous_person = "Naruto"
+quote_1 = "Never Give Up!."
+print(f"{famous_person} once stated, {quote_1}")
+
+v_3 = "   Pippin   "
+print(v_3)
+print(v_3.rstrip())
+print(v_3.lstrip())
+
+
